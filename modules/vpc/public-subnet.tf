da4f38d67565aa7d00c714e71c16c791bc34d195
hemasivakishore <hemasivakishore@gmail.com>
2025-12-06 22:44:40 +0530

    Add public subnet and NAT gateway resources with related variables and outputs

---
+++ modules/vpc/public-subnet.tf
@@ -0,0 +1,15 @@
+resource "aws_subnet" "public-subnet" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = local.public-subnet-cidr
+    availability_zone = local.availability_zone
+    tags = {
+        Name = local.public_subnet_name
+        Owner = local.owner
+        Project = local.project
+        environment = local.environment
+        Terraform_Managed = "true"
+        Managed_By = "terraform"
+        Created_By = "terraform"
+        Subnet_Type = "public"
+    }
+}
