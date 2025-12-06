f25b6fdca28bf30e9a6d7ef4c083989ab7d74772
Hema Siva Kishore <70573794+hemasivakishore@users.noreply.github.com>
2025-12-06 23:32:51 +0530

    Change service_ports to use toset function

--- modules/vpc/security-group.tf
+++ modules/vpc/security-group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "allow-all" {
     vpc_id = aws_vpc.vpc.id
 
     dynamic "ingress" {
-        for_each = var.service_ports
+        for_each = toset(var.service_ports)
         content {
             from_port = ingress.value
             to_port = ingress.value
