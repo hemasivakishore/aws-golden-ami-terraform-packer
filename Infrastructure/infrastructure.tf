e21d67fc43a3adb992ef097f6ec14f230f061447
hemasivakishore <hemasivakishore@gmail.com>
2025-12-11 10:34:43 +0530

    Add initial infrastructure configuration for golden AMI module

---
+++ Infrastructure/infrastructure.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "golden-ami" {
+  source                    = "git::https://github.com/hemasivakishore/aws-golden-ami-terraform-packer.git//modules/vpc?reg=main"
+  vpc_name                  = "golden-ami-vpc"
+  igw_name                  = "golden-ami-igw"
+  owner                     = "hemasivakishore"
+  project                   = "golden-ami"
+  environment               = "golden-ami-env"
+  cidr_block                = "192.168.0.0/16"
+  private_subnet_cidr_name  = "golden-ami-private-subnet"
+  private_subnet_cidr_block = "192.168.1.0/24"
+  public_subnet_cidr_name   = "golden-ami-public-subnet"
+  public_subnet_cidr_block  = "192.168.2.0/24"
+  availability_zone         = "us-east-1a"
+  private_route_table_name  = "golden-ami-private-rt"
+  service_ports             = [22, 80, 443, 3389, 445, 8080]
+  ami                       = "ami-0ecb62995f68bb549"
+  aws_region                = "us-east-1"
+  key_name                  = "vhsk"
+
+}
