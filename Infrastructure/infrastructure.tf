85d9a1781127e46e64e38107f3a16bc3d6581431
hemasivakishore <hemasivakishore@gmail.com>
2025-12-11 11:02:25 +0530

    module url error rectified

--- Infrastructure/infrastructure.tf
+++ Infrastructure/infrastructure.tf
@@ -12,22 +12,21 @@ provider "aws" {
 }
 
 module "golden-ami" {
-  source                    = "git::https://github.com/hemasivakishore/aws-golden-ami-terraform-packer.git//modules/vpc?reg=main"
-  vpc_name                  = "golden-ami-vpc"
-  igw_name                  = "golden-ami-igw"
-  owner                     = "hemasivakishore"
-  project                   = "golden-ami"
-  environment               = "golden-ami-env"
-  cidr_block                = "192.168.0.0/16"
-  private_subnet_cidr_name  = "golden-ami-private-subnet"
-  private_subnet_cidr_block = "192.168.1.0/24"
-  public_subnet_cidr_name   = "golden-ami-public-subnet"
-  public_subnet_cidr_block  = "192.168.2.0/24"
-  availability_zone         = "us-east-1a"
-  private_route_table_name  = "golden-ami-private-rt"
-  service_ports             = [22, 80, 443, 3389, 445, 8080]
-  ami                       = "ami-0ecb62995f68bb549"
-  aws_region                = "us-east-1"
-  key_name                  = "vhsk"
-
+  source                   = "git::https://github.com/hemasivakishore/aws-golden-ami-terraform-packer.git//modules/vpc?ref=main"
+  vpc_name                 = "golden-image"
+  igw_name                 = "golden-igw"
+  owner                    = "golden-ami"
+  project                  = "golden-project"
+  environment              = "golden-env"
+  cidr_block               = "192.168.0.0/16"
+  private_subnet_name      = "golden-private-subnet"
+  private_subnet_cidr      = "192.168.1.0/24"
+  public_subnet_name       = "golden-public-subnet"
+  public_subnet_cidr       = "192.168.2.0/24"
+  availability_zone        = "us-east-1a"
+  private_route_table_name = "golden-private-route-table"
+  service_ports            = [22, 80, 443, 3389, 445, 8080]
+  ami                      = "ami-0ecb62995f68bb549"
+  aws_region               = "us-east-1"
+  key_name                 = "vhsk"
 }
